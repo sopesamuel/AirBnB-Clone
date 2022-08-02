22875a4a24ce814dc0a55e901f0c744fa6d9858b
sopesamuel <fasusimosope@gmail.com>
2022-08-02 23:33:55 +0100

    first commit

--- air-bnb-app/src/App.js
+++ air-bnb-app/src/App.js
@@ -1,18 +1,32 @@
 import React from "react"
 import './App.css';
-import Navbar from "./Components/Navbar.js"
-import Card from "./Components/Card.js"
-import Player1 from "/Users/mac/Documents/Air-BNB Clone/air-bnb-app/src/Components/Player1.js"
-  //<Card />
+import Navbar from "/Users/mac/Documents/Air-BNB Clone/air-bnb-app/src/Components/Navbar.js"
+import Card from "/Users/mac/Documents/Air-BNB Clone/air-bnb-app/src/Components/Card.js"
+import Player1 from "./Components/Player1.js"
+import Data from "/Users/mac/Documents/Air-BNB Clone/air-bnb-app/src/Components/Data.js"
 
 export default function App() {
+const players = Data.map(items =>{
+  return (
+    <Player1 
+    title = {items.title}
+    rating ={items.stats.rating}
+    reviewCount = {items.stats.reviewCount}
+    location = {items.location}
+    price = {items.price}
+    />
+  )
+})
+
   return (
     <div className="App">
       <Navbar />
-    <Player1 />
-      
+    <Card />
+    {players}
     </div>
   )
 }
+
+
 
 
--- air-bnb-app/src/Components/Player1.js
+++ air-bnb-app/src/Components/Player1.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React from "react"
 import Katie from "/Users/mac/Documents/Air-BNB Clone/air-bnb-app/src/images/katie-zaferes.png"
 import Star from "/Users/mac/Documents/Air-BNB Clone/air-bnb-app/src/images/star.png"
  
-export default function Player1(){
-    return(
-        <div className="players">
+
+export default function Player1(props){
+    
+return(
+    <div className="player1">
             <div className="katie">
-                <img src={Katie} /><div className="katie-text"> SOLD OUT </div>
-                 <div className="katie-star-text"><img src={Star} /> <p>5.0 <span className="Light-txt">(6).USA</span></p></div>
-                <div className="katie-star-text2">Life lessons with Katie Zaferes<br>
-                </br><span className="Bold-txt">From $136</span>/ person</div>
+                <img src={Katie} />
+                <div className="katie-text"> SOLD OUT </div>
+                 <div className="katie-star-text"><img src={Star} /> 
+                 <p>{props.rating}<span className="Light-txt">({props.rating}).{props.location}</span></p>
+                 </div>
+                <div className="katie-star-text2">{props.title}<br>
+                </br><span className="Bold-txt">From ${props.price}</span>/ person</div>
             </div>
         </div>
-    )
+)
 }
+
